fd695ef8def1d99acbbab7647e626378cc536d12
廖昱信 <lmly9193@gmail.com>
2016-10-25 05:10:52 +0800

    Version 1.0.1
    https://drive.google.com/open?id=FILEID To
    http://loaclhost/index.php?FILEID

--- input.php
+++ input.php
@@ -2,6 +2,6 @@
 	$jwtitle = "我把你當低能";
 	$jwdescription = "乖乖閉嘴";
 	$jwimage = "https://lh6.googleusercontent.com/uFMo3rzHSdRob5jjs0QBb0gD2gzkXmhFpOQyE37iAC8VUn5OfE2_Xkn9IV7QxwIUnTt80Pb8zbGevDI=w1920-h1080-n-k-rw";
-	$jwfile = "https://drive.google.com/open?id=0B757HvrhbFeiejdzUkZhWVZtaFk";
+	$jwfile = "https://drive.google.com/open?id=".$_SERVER["QUERY_STRING"];
 	$jwautostart ="false";
 ?>
